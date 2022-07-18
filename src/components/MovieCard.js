7f81fc065683e0555c2e60d7d6365f5aa78b2fc0
Budianto IP <budiantoip@gmail.com>
2022-07-18 13:48:34 +0700

    fix rating issue

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -8,30 +8,40 @@ const BASE_IMAGE_URL = "http://image.tmdb.org/t/p/original";
 
 const MovieCard = ({ movie }) => {
   return (
-    <Card id={movie.id} sx={{ display: 'flex', width: 400, margin: 5 }}>
+    <Card id={movie.id} sx={{ display: "flex", width: 400, margin: 5 }}>
       <CardMedia
-        component="img"
+        component='img'
         sx={{ width: 150, height: 225 }}
         image={`${BASE_IMAGE_URL}${movie.poster_path}`}
-        alt="Movie poster"
+        alt='Movie poster'
       />
-      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-        <CardContent sx={{ flex: '1 0 auto' }}>
-          <Typography component="div" variant="h6">
+      <Box sx={{ display: "flex", flexDirection: "column" }}>
+        <CardContent sx={{ flex: "1 0 auto" }}>
+          <Typography component='div' variant='h6'>
             {movie.title}
           </Typography>
-          <Typography variant="subtitle1" color="text.secondary" component="div">
+          <Typography
+            variant='subtitle1'
+            color='text.secondary'
+            component='div'
+          >
             {new Date(movie.release_date).getFullYear()}
           </Typography>
           <Box
             sx={{
               width: 200,
-              display: 'flex',
-              alignItems: 'center',
+              display: "flex",
+              alignItems: "center",
             }}
           >
-            <Rating name="read-only" precision={0.1} value={movie.vote_average / 2} max={5} readOnly />
-            <Box sx={{ ml: 2 }}>{movie.vote_average}</Box>
+            <Rating
+              name='read-only'
+              precision={0.1}
+              value={movie.vote_average / 2}
+              max={5}
+              readOnly
+            />
+            <Box sx={{ ml: 2 }}>{(movie.vote_average / 2).toFixed(2)}</Box>
           </Box>
         </CardContent>
       </Box>
