045b44f84a8276a0784b5ea1db9d2fd16eb7af16
Budianto IP <budiantoip@gmail.com>
2022-07-18 07:56:47 +0700

    change image url

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -4,13 +4,15 @@ import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import * as React from 'react';
 
+const BASE_IMAGE_URL = "https://image.tmdb.org/t/p/original";
+
 const MovieCard = ({ movie }) => {
   return (
     <Card id={movie.id} sx={{ display: 'flex', width: 400, margin: 5 }}>
       <CardMedia
         component="img"
         sx={{ width: 150, height: 225 }}
-        image="http://placekitten.com/500"
+        image={BASE_IMAGE_URL + movie.poster_path}
         alt="Live from space album cover"
       />
       <Box sx={{ display: 'flex', flexDirection: 'column' }}>
